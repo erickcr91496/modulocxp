36e9aeb98a9364c67b05521fb3ec64cdc3b8bc70
erickcr91496 <erick.alexander25@hotmail.com>
2021-03-13 01:20:55 -0500

    visas actualizadas crud Cuentas y Maestro Detalle v2

--- minimarketdemoWeb/src/minimarketdemo/controller/cuentaBancaria/BeanCuentaB.java
+++ minimarketdemoWeb/src/minimarketdemo/controller/cuentaBancaria/BeanCuentaB.java
@@ -21,196 +21,208 @@ import minimarketdemo.model.core.managers.ManagerProveedor;
 @Named
 @SessionScoped
 public class BeanCuentaB implements Serializable {
- 
-    private int codiProv;
-    private String codigoCB;
-    private String tipoCuenta;
-    private String entidadBancaria;
-    private String descripcion;
-    private BigDecimal saldo;
-    private Boolean estado;
-    private CuentaBancaria cuentaEdit;
- 
-    private List<Proveedor> listaProveedores;
-    private List<CuentaBancaria> listaCuentasBancarias;
- 
-    @EJB
-    private ManagerCuentasB mCuentas;
-    @EJB
-    private ManagerProveedor mProveedor;
- 
-    private Proveedor provee;
- 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 1L;
- 
-    public BeanCuentaB() {
-    }
- 
-    @PostConstruct
-    public void inicializar() {
-        listaCuentasBancarias = mCuentas.findAllCuentasBancarias();
-         listaProveedores = mProveedor.ListarProveedores(); 
-         provee = new Proveedor();
-        
-    }
- 
-    public void actionListenerCrearCuenta() {
-        try {
-            System.out.println("actionListenerCrearCuenta!!!!!!!!!");
-            provee = mProveedor.proveedorById(codiProv);
-            System.out.println(provee.getNombre());
-            mCuentas.crearCuentaBancaria(provee.getCodigoprov(), provee.getNombre(), tipoCuenta, entidadBancaria,
-                    descripcion, saldo, true);
-            JSFUtil.crearMensajeINFO("Cuenta creada");
-            // actualizamos la lista de cuentas
-            listaCuentasBancarias = mCuentas.findAllCuentasBancarias();
-            codigoCB = "";
-            tipoCuenta = "";
-            entidadBancaria = "";
-            descripcion = "";
- 
-            provee = new Proveedor();
- 
-        } catch (Exception e) {
-            JSFUtil.crearMensajeERROR(e.getMessage());
-            e.printStackTrace();
-        }
- 
-    }
- 
-    public void actionListenerSeleccionarCuenta(CuentaBancaria cuenta) {
-        cuentaEdit = cuenta;
-        System.out.println("cuenta seleccionado: " + cuentaEdit.getCodigocb());
- 
-    }
- 
-    public void actionListenerActualizarCuenta() {
-        try {
-            provee = mProveedor.proveedorById(codiProv);
-            cuentaEdit.setCodigoprov(provee.getCodigoprov());
-            cuentaEdit.setNombre(provee.getNombre());
-            mCuentas.actualizarCuentaBancaria(cuentaEdit);
-            listaCuentasBancarias = mCuentas.findAllCuentasBancarias();
-            JSFUtil.crearMensajeINFO("Cuenta actualizada");
- 
-        } catch (Exception e) {
-            JSFUtil.crearMensajeERROR(e.getMessage());
-            e.printStackTrace();
-        }
-    }
- 
-    public void actionListenerEliminarCuenta(String codigo) {
-        try {
-            mCuentas.eliminarCuentaBancaria(codigo);
-            JSFUtil.crearMensajeINFO("la cuenta se ha eliminado");
-            listaCuentasBancarias = mCuentas.findAllCuentasBancarias();
- 
-        } catch (Exception e) {
- 
-            JSFUtil.crearMensajeERROR(e.getMessage());
-            e.printStackTrace();
-        }
-    }
- 
-    public String actionSeleccionarEdicionCuenta(CuentaBancaria cuentab) {
-        cuentaEdit = cuentab;
-        System.out.println("cuenta seleccionado: " + cuentaEdit.getCodigocb());
-        return "cuentas_edicion";
-    }
- 
-    public String getCodigoCB() {
-        return codigoCB;
-    }
- 
-    public void setCodigoCB(String codigoCB) {
-        this.codigoCB = codigoCB;
-    }
- 
-    public int getCodiProv() {
-        return codiProv;
-    }
- 
-    public void setCodiProv(int codiProv) {
-        this.codiProv = codiProv;
-    }
- 
-    public Proveedor getProvee() {
-        return provee;
-    }
- 
-    public void setProvee(Proveedor provee) {
-        this.provee = provee;
-    }
- 
-    public CuentaBancaria getCuentaEdit() {
-        return cuentaEdit;
-    }
- 
-    public void setCuentaEdit(CuentaBancaria cuentaEdit) {
-        this.cuentaEdit = cuentaEdit;
-    }
- 
-    public List<Proveedor> getListaProveedores() {
-        return listaProveedores;
-    }
- 
-    public void setListaProveedores(List<Proveedor> listaProveedores) {
-        this.listaProveedores = listaProveedores;
-    }
- 
-    public List<CuentaBancaria> getListaCuentasBancarias() {
-        return listaCuentasBancarias;
-    }
- 
-    public void setListaCuentasBancarias(List<CuentaBancaria> listaCuentasBancarias) {
-        this.listaCuentasBancarias = listaCuentasBancarias;
-    }
- 
-    public String getTipoCuenta() {
-        return tipoCuenta;
-    }
- 
-    public void setTipoCuenta(String tipoCuenta) {
-        this.tipoCuenta = tipoCuenta;
-    }
- 
-    public String getEntidadBancaria() {
-        return entidadBancaria;
-    }
- 
-    public void setEntidadBancaria(String entidadBancaria) {
-        this.entidadBancaria = entidadBancaria;
-    }
- 
-    public String getDescripcion() {
-        return descripcion;
-    }
- 
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
- 
-    public BigDecimal getSaldo() {
-        return saldo;
-    }
- 
-    public void setSaldo(BigDecimal saldo) {
-        this.saldo = saldo;
-    }
- 
-    public Boolean getEstado() {
-        return estado;
-    }
- 
-    public void setEstado(Boolean estado) {
-        this.estado = estado;
-    }
- 
-    public static long getSerialversionuid() {
-        return serialVersionUID;
-    }
- 
+
+
+	private String nombre;
+	private String tipoCuenta;
+	private String entidadBancaria;
+	private String descripcion;
+	private BigDecimal saldo;
+	private Boolean estado;
+	private Cuentabancaria cuentaEdit;
+
+
+	private List<Cuentabancaria> listaCuentasBancarias;
+
+	@EJB
+	private ManagerCuentasB mCuentas;
+	@EJB
+	private ManagerProveedorFacturas mProveedor;
+
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BeanCuentaB() {
+	}
+
+	@PostConstruct
+	public void inicializar() {
+		listaCuentasBancarias = mCuentas.findAllCuentasBancarias();
+	}
+
+	public void actionListenerCrearCuenta() {
+		try {
+
+
+			mCuentas.crearCuentaBancaria(nombre, tipoCuenta, entidadBancaria, descripcion, saldo, true);
+			JSFUtil.crearMensajeINFO("Cuenta creada");
+			//actualizamos la lista de cuentas
+			listaCuentasBancarias= mCuentas.findAllCuentasBancarias();
+			nombre="";
+			tipoCuenta="";
+			entidadBancaria="";
+			descripcion="";
+			
+			
+
+			System.out.println("actionListenerCrearCuenta!!!!!!!!!");
+			mCuentas.crearCuentaBancaria(nombre, tipoCuenta, entidadBancaria, descripcion, saldo, true);
+			JSFUtil.crearMensajeINFO("Cuenta creada");
+			// actualizamos la lista de cuentas
+			listaCuentasBancarias = mCuentas.findAllCuentasBancarias();
+			nombre = "";
+			tipoCuenta = "";
+			entidadBancaria = "";
+			descripcion = "";
+
+			provee = new Proveedor();
+
+			mCuentas.crearCuentaBancaria(nombre, tipoCuenta, entidadBancaria, descripcion, saldo, true);
+			JSFUtil.crearMensajeINFO("Cuenta creada");
+			//actualizamos la lista de cuentas
+			listaCuentasBancarias= mCuentas.findAllCuentasBancarias();
+			nombre="";
+			tipoCuenta="";
+			entidadBancaria="";
+			descripcion="";
+			
+			
+
+		} catch (Exception e) {
+			JSFUtil.crearMensajeERROR(e.getMessage());
+			e.printStackTrace();
+		}
+
+	}
+
+	public void actionListenerSeleccionarCuenta(Cuentabancaria cuenta) {
+		cuentaEdit = cuenta;
+		System.out.println("cuenta seleccionado: " + cuentaEdit.getCodigocb());
+
+	}
+
+	public void actionListenerActualizarCuenta() {
+		try {
+			mCuentas.actualizarCuentaBancaria(cuentaEdit);
+			listaCuentasBancarias = mCuentas.findAllCuentasBancarias();
+			JSFUtil.crearMensajeINFO("Cuenta actualizada");
+
+		} catch (Exception e) {
+			JSFUtil.crearMensajeERROR(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	
+	public String actionSeleccionarEdicionCuenta(CuentaBancaria cuentab) {
+		cuentaEdit=cuentab;
+		System.out.println("cuenta seleccionado: "+cuentaEdit.getCodigocb());
+		return "cuentas_edicion";
+	}
+
+	
+	public String actionSeleccionarEdicionCuenta(CuentaBancaria cuentab) {
+		cuentaEdit=cuentab;
+		System.out.println("cuenta seleccionado: "+cuentaEdit.getCodigocb());
+		return "cuentas_edicion";
+	}
+	
+	public void actionListenerEliminarCuenta(String codigo) {
+		try {
+			mCuentas.eliminarCuentaBancaria(codigo);
+			JSFUtil.crearMensajeINFO("la cuenta se ha eliminado");
+			listaCuentasBancarias = mCuentas.findAllCuentasBancarias();
+
+		} catch (Exception e) {
+
+			JSFUtil.crearMensajeERROR(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	public String actionSeleccionarEdicionCuenta(Cuentabancaria cuentab) {
+		cuentaEdit = cuentab;
+		System.out.println("cuenta seleccionado: " + cuentaEdit.getCodigocb());
+		return "cuentas_edicion";
+	}
+	
+
+	
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public Cuentabancaria getCuentaEdit() {
+		return cuentaEdit;
+	}
+
+	public void setCuentaEdit(Cuentabancaria cuentaEdit) {
+		this.cuentaEdit = cuentaEdit;
+	}
+
+	
+
+	public List<Cuentabancaria> getListaCuentasBancarias() {
+		return listaCuentasBancarias;
+	}
+
+	public void setListaCuentasBancarias(List<Cuentabancaria> listaCuentasBancarias) {
+		this.listaCuentasBancarias = listaCuentasBancarias;
+	}
+
+	public String getTipoCuenta() {
+		return tipoCuenta;
+	}
+
+	public void setTipoCuenta(String tipoCuenta) {
+		this.tipoCuenta = tipoCuenta;
+	}
+
+	public String getEntidadBancaria() {
+		return entidadBancaria;
+	}
+
+	public void setEntidadBancaria(String entidadBancaria) {
+		this.entidadBancaria = entidadBancaria;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public BigDecimal getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(BigDecimal saldo) {
+		this.saldo = saldo;
+	}
+
+	public Boolean getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 }
