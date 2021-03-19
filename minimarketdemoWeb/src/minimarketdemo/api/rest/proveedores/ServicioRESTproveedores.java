243520ad8f223f8e9a9ae205796b45d4cc729637
maralex9 <mafernandezc@utn.edu.ec>
2021-03-18 21:41:52 -0500

    Servicio API

---
+++ minimarketdemoWeb/src/minimarketdemo/api/rest/proveedores/ServicioRESTproveedores.java
@@ -0,0 +1,14 @@
+package minimarketdemo.api.rest.proveedores;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@RequestScoped
+@Path("proveedores")
+@Produces({ "application/xml", "application/json" })
+@Consumes({ "application/xml", "application/json" })
+public class ServicioRESTproveedores {
+
+}
